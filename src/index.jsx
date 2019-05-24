fc52081848b1fca0aa8c91e1bb7815ec6ad01827
Wahaj Aayani <wahaj_aayani@hotmail.com>
2019-05-25 00:43:25 +0500

    code updates

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import Quagga from 'quagga';
-import PropTypes from 'prop-types';
+import React from "react";
+import Quagga from "quagga";
+import PropTypes from "prop-types";
 
-const defaultId = '__process_div';
+const defaultId = "__process_div";
 const defaultOption = {
   frequency: 5,
   numOfWorkers: window.navigator.hardwareConcurrency || 4,
-  name: 'Live',
-  type: 'LiveStream',
-  readers: ['ean_reader']
+  name: "Live",
+  type: "LiveStream",
+  readers: ["ean_reader"]
 };
 
 const isMediaSupported =
   window.navigator.mediaDevices &&
-  typeof window.navigator.mediaDevices.getUserMedia === 'function';
+  typeof window.navigator.mediaDevices.getUserMedia === "function";
 
 const getResponse = ({
   codeResult: { code: result, direction, format },
@@ -47,8 +47,8 @@ class Component extends React.Component {
     const { onDetected, type } = this.props;
 
     if (isMediaSupported) {
-      if (type === 'barcode') {
-        this.initializeQuagga((err) => {
+      if (type === "barcode") {
+        this.initializeQuagga(err => {
           if (this.mounted) {
             if (err) {
               this.setState({
@@ -67,21 +67,21 @@ class Component extends React.Component {
         });
 
         Quagga.onProcessed(() => {
-          Quagga.canvas.dom.overlay.style.display = 'none';
+          Quagga.canvas.dom.overlay.style.display = "none";
         });
 
-        if (typeof onDetected === 'function') {
-          Quagga.onDetected((data) => onDetected(getResponse(data)));
+        if (typeof onDetected === "function") {
+          Quagga.onDetected(data => onDetected(getResponse(data)));
         } else if (this.mounted) {
           this.setState({
             loading: false,
-            error: new Error('onDetected() is not a valid function')
+            error: new Error("onDetected() is not a valid function")
           });
         }
       } else if (this.mounted) {
         this.setState({
           loading: false,
-          error: new Error('No valid scan type is specified')
+          error: new Error("No valid scan type is specified")
         });
       }
     } else if (this.mounted) {
@@ -110,7 +110,7 @@ class Component extends React.Component {
       } else if (initialized) {
         if (power !== status) {
           if (status === true) {
-            this.initializeQuagga((err) => {
+            this.initializeQuagga(err => {
               if (this.mounted) {
                 if (err) {
                   this.setState({
@@ -156,7 +156,7 @@ class Component extends React.Component {
         constraints: {
           width,
           height,
-          facingMode: !deviceId && 'environment',
+          facingMode: !deviceId && "environment",
           deviceId
         },
         locator: {
@@ -177,12 +177,12 @@ class Component extends React.Component {
 
   render() {
     const { loading } = this.state;
-    const { id, className, style } = this.props;
+    const { id, ...props } = this.props;
 
     return (
-      <div className={className} style={style}>
-        {loading && <p style={{ textAlign: 'center' }}>Loading...</p>}
-        <div id={id} style={{ display: loading && 'none' }} />
+      <div {...props}>
+        {loading && <p style={{ textAlign: "center" }}>Loading...</p>}
+        <div id={id} style={{ display: loading && "none" }} />
       </div>
     );
   }
@@ -190,34 +190,32 @@ class Component extends React.Component {
 
 Component.propTypes = {
   id: PropTypes.string,
-  type: PropTypes.oneOf(['barcode', 'qrcode']),
+  type: PropTypes.oneOf(["barcode", "qrcode"]),
   status: PropTypes.bool.isRequired,
   height: PropTypes.number,
   width: PropTypes.number,
   deviceId: PropTypes.string,
-  style: PropTypes.object,
-  className: PropTypes.string,
   option: PropTypes.shape({
     frequency: PropTypes.number,
     numOfWorkers: PropTypes.number,
     name: PropTypes.string,
-    type: PropTypes.oneOf(['ImageStream', 'VideoStream', 'LiveStream']),
+    type: PropTypes.oneOf(["ImageStream", "VideoStream", "LiveStream"]),
     readers: PropTypes.arrayOf(
       PropTypes.oneOf([
-        '2of5_reader',
-        'codabar_reader',
-        'code_39_reader',
-        'code_39_vin_reader',
-        'code_93_reader',
-        'code_128_reader',
-        'ean_reader',
-        'ean_2_reader',
-        'ean_5_reader',
-        'ean_8_reader',
-        'ean_13_reader',
-        'i2of5_reader',
-        'upc_reader',
-        'upc_e_reader'
+        "2of5_reader",
+        "codabar_reader",
+        "code_39_reader",
+        "code_39_vin_reader",
+        "code_93_reader",
+        "code_128_reader",
+        "ean_reader",
+        "ean_2_reader",
+        "ean_5_reader",
+        "ean_8_reader",
+        "ean_13_reader",
+        "i2of5_reader",
+        "upc_reader",
+        "upc_e_reader"
       ])
     )
   }),
@@ -227,14 +225,12 @@ Component.propTypes = {
 
 Component.defaultProps = {
   id: defaultId,
-  type: 'barcode',
+  type: "barcode",
   height: 480,
   width: 640,
   deviceId: undefined,
-  style: undefined,
-  className: undefined,
   option: defaultOption,
-  onError: (err) => console.error(err)
+  onError: err => console.error(err)
 };
 
 export default Component;
